c10e86145133d998dc0f64106f4c4c9c987f9ecc
Igor Goroshit <igor@webt.co.il>
2017-03-10 16:07:52 +0200

    add configurations

--- addon/configuration.js
+++ addon/configuration.js
@@ -3,86 +3,30 @@ import Ember from 'ember';
 const { getWithDefault, typeOf, deprecate } = Ember;
 
 const DEFAULTS = {
-  baseURL:                     '',
-  authenticationRoute:         'login',
-  routeAfterAuthentication:    'index',
-  routeIfAlreadyAuthenticated: 'index'
+  baseURL:                      '',
+  authenticationRoute:          'login',
+  routeAfterAuthentication:     'index',
+  routeIfAlreadyAuthenticated:  'index',
+  refreshAfterInvalidation:     false,
+  serverTokenEndpoint:          'login',
+  tokenAttributeName:           'token',
+  identificationAttributeName:  'email',
+  cookieName:                   'ember-auth:session',
+  localStorageKey:              'ember-auth:session',
 };
 
-/**
-  Ember Simple Auth's configuration object.
-
-  To change any of these values, set them on the application's environment
-  object, e.g.:
-
-  ```js
-  // config/environment.js
-  ENV['ember-simple-auth'] = {
-    baseURL: 'path/to/base/url'
-  };
-  ```
-
-  @class Configuration
-  @extends Object
-  @module ember-simple-auth/configuration
-  @public
-*/
 export default {
-  /**
-    The base URL of the application as configured in `config/environment.js`.
-
-    @property baseURL
-    @readOnly
-    @static
-    @type String
-    @default ''
-    @public
-  */
-  baseURL: DEFAULTS.baseURL,
-
-  /**
-    The route to transition to for authentication. The
-    {{#crossLink "AuthenticatedRouteMixin"}}{{/crossLink}} will transition to
-    this route when a route that implements the mixin is accessed when the
-    route is not authenticated.
-
-    @property authenticationRoute
-    @deprecated AuthenticatedRouteMixin/authenticationRoute:property
-    @readOnly
-    @static
-    @type String
-    @default 'login'
-    @public
-  */
-  authenticationRoute: DEFAULTS.authenticationRoute,
-
-  /**
-    The route to transition to after successful authentication.
-
-    @property routeAfterAuthentication
-    @deprecated ApplicationRouteMixin/routeAfterAuthentication:property
-    @readOnly
-    @static
-    @type String
-    @default 'index'
-    @public
-  */
-  routeAfterAuthentication: DEFAULTS.routeAfterAuthentication,
-
-  /**
-    The route to transition to if a route that implements the
-    {{#crossLink "UnauthenticatedRouteMixin"}}{{/crossLink}} is accessed when
-    the session is authenticated.
-
-    @property routeIfAlreadyAuthenticated
-    @deprecated UnauthenticatedRouteMixin/routeIfAlreadyAuthenticated:property
-    @readOnly
-    @static
-    @type String
-    @default 'index'
-    @public
-  */
-  routeIfAlreadyAuthenticated: DEFAULTS.routeIfAlreadyAuthenticated,
+  
+  baseURL:                      DEFAULTS.baseURL,
+  authenticationRoute:          DEFAULTS.authenticationRoute,
+  routeAfterAuthentication:     DEFAULTS.routeAfterAuthentication,
+  routeIfAlreadyAuthenticated:  DEFAULTS.routeIfAlreadyAuthenticated,
+  refreshAfterInvalidation:     DEFAULTS.refreshAfterInvalidation,
+  serverTokenEndpoint:          DEFAULTS.serverTokenEndpoint,
+  tokenAttributeName:           DEFAULTS.tokenAttributeName,
+  identificationAttributeName:  DEFAULTS.identificationAttributeName,
+  cookieName:                   DEFAULTS.cookieName,
+  localStorageKey:              DEFAULTS.localStorageKey,
 
   load(config) {
     for (let property in this) {
