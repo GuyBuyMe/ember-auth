b1f3a304f583f73114321e885123ba58141b6323
Igor Goroshit <igor@webt.co.il>
2017-03-15 00:12:45 +0200

    improve configurations

--- addon/configuration.js
+++ addon/configuration.js
@@ -3,30 +3,56 @@ import Ember from 'ember';
 const { getWithDefault, typeOf, deprecate } = Ember;
 
 const DEFAULTS = {
+
   baseURL:                      '',
   authenticationRoute:          'login',
   routeAfterAuthentication:     'index',
   routeIfAlreadyAuthenticated:  'index',
   refreshAfterInvalidation:     false,
-  serverTokenEndpoint:          'login',
-  tokenAttributeName:           'token',
+
   identificationAttributeName:  'email',
+  identificationField:          'username',
+  passwordField: 'password',
+
   cookieName:                   'ember-auth:session',
   localStorageKey:              'ember-auth:session',
+
+  tokenAttributeName:           'token',
+  tokenPropertyName:            'token',
+  tokenExpireName:              'exp',
+  serverTokenEndpoint:          'login/token',
+  serverTokenRefreshEndpoint:   'login/token/refresh',
+  refreshAccessTokens:          false,
+  refreshLeeway:                0,
+
+  authorizationPrefix:          'Bearer ',
+  authorizationHeaderName:      'Authorization',
+  headers:                      {},
+
 };
 
 export default {
   
-  baseURL:                      DEFAULTS.baseURL,
-  authenticationRoute:          DEFAULTS.authenticationRoute,
-  routeAfterAuthentication:     DEFAULTS.routeAfterAuthentication,
-  routeIfAlreadyAuthenticated:  DEFAULTS.routeIfAlreadyAuthenticated,
-  refreshAfterInvalidation:     DEFAULTS.refreshAfterInvalidation,
-  serverTokenEndpoint:          DEFAULTS.serverTokenEndpoint,
-  tokenAttributeName:           DEFAULTS.tokenAttributeName,
-  identificationAttributeName:  DEFAULTS.identificationAttributeName,
-  cookieName:                   DEFAULTS.cookieName,
-  localStorageKey:              DEFAULTS.localStorageKey,
+  baseURL:                      DEFAULTs.baseURL,
+  authenticationRoute:          DEFAULTs.authenticationRoute,
+  routeAfterAuthentication:     DEFAULTs.routeAfterAuthentication,
+  routeIfAlreadyAuthenticated:  DEFAULTs.routeIfAlreadyAuthenticated,
+  refreshAfterInvalidation:     DEFAULTs.refreshAfterInvalidation,
+  identificationAttributeName:  DEFAULTs.identificationAttributeName,
+  identificationField:          DEFAULTs.identificationField,
+  passwordField:                DEFAULTs.passwordField,
+  cookieName:                   DEFAULTs.cookieName,
+  localStorageKey:              DEFAULTs.localStorageKey,
+  tokenAttributeName:           DEFAULTs.tokenAttributeName,
+  tokenPropertyName:            DEFAULTs.tokenPropertyName,
+  tokenExpireName:              DEFAULTs.tokenExpireName,
+  serverTokenEndpoint:          DEFAULTs.serverTokenEndpoint,
+  serverTokenRefreshEndpoint:   DEFAULTs.serverTokenRefreshEndpoint,
+  refreshAccessTokens:          DEFAULTs.refreshAccessTokens,
+  refreshLeeway:                DEFAULTs.refreshLeeway,
+  authorizationPrefix:          DEFAULTs.authorizationPrefix,
+  authorizationHeaderName:      DEFAULTs.authorizationHeaderName,
+  headers:                      DEFAULTs.headers,
 
   load(config) {
     for (let property in this) {
