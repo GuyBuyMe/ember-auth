6d14805fe76b3287f0191a2f6614c979afe07f5d
Guy <104722328+GuyBuyMe@users.noreply.github.com>
2024-06-27 17:56:48 +0300

    Update local-storage.js

--- addon/session-stores/local-storage.js
+++ addon/session-stores/local-storage.js
@@ -40,7 +40,7 @@ export default BaseStore.extend({
     @default 'ember_simple_auth-session'
     @public
   */
-  key: 'ember_simple_auth-session',
+  key: ENV['ember-auth']['localStorageKey],
 
   init() {
     this._super(...arguments);
